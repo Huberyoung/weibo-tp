0d27f944e7fd3465d4f9c07616ad1779ca8058e2
huberyoung <Huberyoung@163.com>
2019-04-16 22:57:31 +0800

    数据库迁移

---
+++ database/migrations/20190416131251_create_users_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use think\migration\Migrator;
+use think\migration\db\Column;
+
+class CreateUsersTable extends Migrator
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function up(){
+        $table = $this->table('users');
+        $table->addColumn('username' , 'string' , ['limit' => 30])
+            ->addColumn('passwork' , 'string' , ['limit' => 32])
+            ->addColumn('email' , 'string' , ['limit' => 25])
+            ->addColumn('lastlogin_ip' , 'string' , ['limit' => 15])
+            ->addTimestamps('create_time' , 'lastlogin_time')
+            ->addColumn('status' , 'integer' , ['limit' => 1 , 'default' => 1])
+            ->setId('user_id')
+            ->save();
+
+    }
+
+    /**
+     * 提供回滚的删除用户表方法
+     */
+    public function down(){
+        $this->dropTable('users');
+    }
+}
